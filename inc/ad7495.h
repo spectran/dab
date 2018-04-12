4544194a6293a1b06e0754295a2258b7262f73ef
spectran <terentyev_ruslan@mail.ru>
2018-04-12 23:17:22 +0300

    Add files via upload

---
+++ inc/ad7495.h
@@ -0,0 +1,14 @@
+/*
+ * ad7495.h
+ *
+ *  Created on: 24 сент. 2016 г.
+ *      Author: Spectran
+ */
+
+#ifndef AD7495_H_
+#define AD7495_H_
+
+void Read_Adc();
+void Adc_Probe();
+
+#endif /* AD7495_H_ */
